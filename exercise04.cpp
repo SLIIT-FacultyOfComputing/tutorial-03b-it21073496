9add14374d21e5757c0f7b645e8648abe48b0969
it21073496 <87603949+it21073496@users.noreply.github.com>
2021-07-25 11:26:40 +0000

    Done.

--- exercise04.cpp
+++ exercise04.cpp
@@ -19,3 +19,10 @@ void print(int len, int wth) {
 }
 
 // Implement the Input Function here
+void input(int len, int wth)
+{
+  cout << "Input Length : ";
+  cin >> len;
+  cout << "Input Width  : ";
+  cin >> wth;
+}
